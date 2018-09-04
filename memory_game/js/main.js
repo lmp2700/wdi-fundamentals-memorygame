e598d85ac847b4f4f54690c6b1c9b7ad2710e5ae
Liz Steak <lizmaepark@gmail.com>
2018-09-03 22:44:52 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+	},
+	{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+	},
+	{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = []; 
 
 var checkForMatch = function () {
@@ -11,18 +33,27 @@ var checkForMatch = function () {
 // checkForMatch function is over-riding calling checkForMatch in 
 // flipCard function (??)
 // stating that king & queen are a match when they are not
-// console.log in flipCard function is pulling correct cards
+// all console.log in flipCard function are pulling correct cards
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[0]);
-	console.log("User flipped " + cards[2]); 
+	console.log("User flipped " + cards[0].rank);
+	console.log(cards[0].cardImage);
+	console.log(cards[0].suit);
+
+	console.log("User flipped " + cards[2].rank);
+	console.log(cards[2].cardImage);
+	console.log(cards[2].suit);
+	
 	checkForMatch();
 } 
 
 flipCard();
 
-cardsInPlay.push(cards[0]);
-cardsInPlay.push(cards[2]);
+cardsInPlay.push(cards[0].rank);
+cardsInPlay.push(cards[2].rank);
+
+
+
 
 
 /* 	DELETE THESE LINES OF CODE:
