0ef17e3b5b0420d1f9e8c065895ff338debf91f7
Liz Steak <lizmaepark@gmail.com>
2018-09-04 01:52:41 -0600

    finished game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,43 +24,42 @@ cardImage: "images/king-of-diamonds.png"
 var cardsInPlay = []; 
 
 var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage); 
+	
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
 } else {
-	console.log("Sorry, try again.");
+		alert("Sorry, try again.");
+		}
 	}
-}
-// checkForMatch function is over-riding calling checkForMatch in 
-// flipCard function (??)
-// stating that king & queen are a match when they are not
-// all console.log in flipCard function are pulling correct cards
+};
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[0].rank);
-	console.log(cards[0].cardImage);
-	console.log(cards[0].suit);
-
-	console.log("User flipped " + cards[2].rank);
-	console.log(cards[2].cardImage);
-	console.log(cards[2].suit);
-	
-	checkForMatch();
-} 
-
-flipCard();
-
-cardsInPlay.push(cards[0].rank);
-cardsInPlay.push(cards[2].rank);
+var flipCard = function () {
+	var cardId = this.getAttribute("data-id");
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	checkForMatch.call(this);
+};
 
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
 
 
-
-/* 	DELETE THESE LINES OF CODE:
-	var cardOne = cards[0];
-	cardsInPlay.push(cardOne);
-	console.log("User flipped " + cardOne);
-	
-	var cardTwo = cards[3];
-	cardsInPlay.push(cardTwo);
-	console.log("User flipped " + cardTwo); */
+// It might be useful to add a "Reset" button to the HTML and set 
+// up an event in the JavaScript file so that the user can reset the 
+// game after playing.
+// For an extra challenge, consider how you could keep track of and 
+// display the user's score.
